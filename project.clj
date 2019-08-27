ba93f1eba4e9735d14befff71d445e7ef2611421
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-08-27 12:18:25 +0200

    Task 5: Implemented calendar component in common-client namespace.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/ide-middle "0.2.17"
+(defproject org.clojars.vladimirmarkovic86/ide-middle "0.2.18"
   :description "Integrated development environment middleware"
   :url "http://github.com/VladimirMarkovic86/ide-middle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.12"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.13"]
                  ]
 
   :min-lein-version "2.0.0"
